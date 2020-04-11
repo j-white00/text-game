7ac440ff9d6de902b2f8e1fa9432bf1bb4a414c5
Jordan White <jordanwhite@Jordans-MacBook-Pro.local>
2020-04-11 13:56:43 +0100

    Video and Success screen added

--- Website/js/SuccessPageScript.js
+++ Website/js/SuccessPageScript.js
@@ -1,3 +1,9 @@
 var time = window.localStorage.getItem('timeRemaining');
 
 $('#stats').append("Time remaining "+time);
+
+setTimeout(function () {
+  document.getElementById("title").style.borderRight = "none";
+  $('#bigTitle').append("<h1>CONFINEMENT</h1>");
+
+}, 3000);
